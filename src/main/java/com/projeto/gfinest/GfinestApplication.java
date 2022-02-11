8cae0b94e75f662ad75aac6cc7c27822f5353bfb
Marcelo Ferreira <marcelo.ferreira1027@gmail.com>
2022-02-11 17:55:15 -0300

    Project created

---
+++ src/main/java/com/projeto/gfinest/GfinestApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gfinest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfinestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfinestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/gfinest/GfinestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.gfinest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfinestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
